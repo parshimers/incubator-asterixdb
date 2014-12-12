32a5cb17b52c32554a1aac735b0bd9117c6fbb7c
Ian <imaxon@uci.edu>
2014-12-12 11:49:32 -0800

    removed un-necessary guard

--- asterix-app/src/main/java/edu/uci/ics/asterix/result/ResultUtils.java
+++ asterix-app/src/main/java/edu/uci/ics/asterix/result/ResultUtils.java
@@ -252,12 +252,9 @@ public class ResultUtils {
     private static String extractErrorMessage(Throwable e) {
         Throwable cause = getRootCause(e);
         String fullyQualifiedExceptionClassName = cause.getClass().getName();
-        String exceptionClassName = fullyQualifiedExceptionClassName; //safe default if no class qualification
-        //now try to get class name that isn't qualified to output
         String[] hierarchySplits = fullyQualifiedExceptionClassName.split("\\.");
-        if (hierarchySplits.length > 0) {
-                exceptionClassName = hierarchySplits[hierarchySplits.length - 1];
-        }
+        //try returning the class without package qualification
+        String exceptionClassName = hierarchySplits[hierarchySplits.length - 1];
         String localizedMessage = cause.getLocalizedMessage();
         if(localizedMessage == null){
             localizedMessage = "Internal error. Please check instance logs for further details.";
