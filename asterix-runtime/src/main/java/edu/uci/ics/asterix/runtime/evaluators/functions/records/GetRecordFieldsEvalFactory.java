ea3aa898869300fb0e27bdad54a412a874da9e0e
Eldon Carman <ecarm002@ucr.edu>
2015-05-27 14:50:51 -0700

    finished get-record-fields

--- asterix-runtime/src/main/java/edu/uci/ics/asterix/runtime/evaluators/functions/records/GetRecordFieldsEvalFactory.java
+++ asterix-runtime/src/main/java/edu/uci/ics/asterix/runtime/evaluators/functions/records/GetRecordFieldsEvalFactory.java
@@ -14,15 +14,21 @@
  */
 package edu.uci.ics.asterix.runtime.evaluators.functions.records;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 import edu.uci.ics.asterix.builders.IARecordBuilder;
+import edu.uci.ics.asterix.builders.IAsterixListBuilder;
 import edu.uci.ics.asterix.builders.OrderedListBuilder;
 import edu.uci.ics.asterix.builders.RecordBuilder;
 import edu.uci.ics.asterix.common.exceptions.AsterixException;
 import edu.uci.ics.asterix.formats.nontagged.AqlSerializerDeserializerProvider;
+import edu.uci.ics.asterix.om.base.ABoolean;
 import edu.uci.ics.asterix.om.base.AMutableString;
 import edu.uci.ics.asterix.om.base.ANull;
 import edu.uci.ics.asterix.om.base.AString;
@@ -34,6 +40,7 @@ import edu.uci.ics.asterix.om.types.ARecordType;
 import edu.uci.ics.asterix.om.types.ATypeTag;
 import edu.uci.ics.asterix.om.types.BuiltinType;
 import edu.uci.ics.asterix.om.types.EnumDeserializer;
+import edu.uci.ics.asterix.om.types.IAType;
 import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
 import edu.uci.ics.hyracks.algebricks.runtime.base.ICopyEvaluator;
 import edu.uci.ics.hyracks.algebricks.runtime.base.ICopyEvaluatorFactory;
@@ -56,7 +63,6 @@ public class GetRecordFieldsEvalFactory implements ICopyEvaluatorFactory {
     public GetRecordFieldsEvalFactory(ICopyEvaluatorFactory recordEvalFactory, ARecordType recordType) {
         this.recordEvalFactory = recordEvalFactory;
         this.recordType = recordType;
-
     }
 
     @Override
@@ -67,27 +73,41 @@ public class GetRecordFieldsEvalFactory implements ICopyEvaluatorFactory {
             private final AString isOpenName = new AString("is-open");
             private final AString nestedName = new AString("nested");
 
-            protected AMutableString aString = new AMutableString("");
-            @SuppressWarnings("unchecked")
-            protected ISerializerDeserializer<AString> stringSerde = AqlSerializerDeserializerProvider.INSTANCE
-                    .getSerializerDeserializer(BuiltinType.ASTRING);
+            private final Queue<ArrayBackedValueStorage> baaosPool = new ArrayDeque<ArrayBackedValueStorage>();
+            private final Queue<IARecordBuilder> recordBuilderPool = new ArrayDeque<IARecordBuilder>();
+            private final Queue<IAsterixListBuilder> orderedListBuilderPool = new ArrayDeque<IAsterixListBuilder>();
+            private final PointableAllocator pa = new PointableAllocator();
 
-            private DataOutput out = output.getDataOutput();
-
-            private OrderedListBuilder orderedListBuilder = new OrderedListBuilder();
             private final AOrderedListType listType = new AOrderedListType(BuiltinType.ANY, "fields");
+            private final AMutableString aString = new AMutableString("");
+            @SuppressWarnings("unchecked")
+            private final ISerializerDeserializer<ANull> nullSerde = AqlSerializerDeserializerProvider.INSTANCE
+                    .getSerializerDeserializer(BuiltinType.ANULL);
+            @SuppressWarnings("unchecked")
+            protected final ISerializerDeserializer<AString> stringSerde = AqlSerializerDeserializerProvider.INSTANCE
+                    .getSerializerDeserializer(BuiltinType.ASTRING);
+            @SuppressWarnings("unchecked")
+            protected final ISerializerDeserializer<ABoolean> booleanSerde = AqlSerializerDeserializerProvider.INSTANCE
+                    .getSerializerDeserializer(BuiltinType.ABOOLEAN);
 
             private ArrayBackedValueStorage outInput0 = new ArrayBackedValueStorage();
             private ICopyEvaluator eval0 = recordEvalFactory.createEvaluator(outInput0);
-            @SuppressWarnings("unchecked")
-            private ISerializerDeserializer<ANull> nullSerde = AqlSerializerDeserializerProvider.INSTANCE
-                    .getSerializerDeserializer(BuiltinType.ANULL);
 
-            private final PointableAllocator pa = new PointableAllocator();
-            private ARecordPointable recordPointable;
+            private DataOutput out = output.getDataOutput();
+            private ATypeTag tag;
+            private AString currentFieldName;
+            private ARecordType newType;
 
+            private ARecordType openType;
             {
                 recordType = recordType.deepCopy(recordType);
+                try {
+                    openType = new ARecordType("open", new String[0], new IAType[0], true);
+                } catch (HyracksDataException e) {
+                    e.printStackTrace();
+                } catch (AsterixException e) {
+                    e.printStackTrace();
+                }
             }
 
             public void evaluate(IFrameTupleReference tuple) throws AlgebricksException {
@@ -107,13 +127,11 @@ public class GetRecordFieldsEvalFactory implements ICopyEvaluatorFactory {
                             + EnumDeserializer.ATYPETAGDESERIALIZER.deserialize(outInput0.getByteArray()[0]));
                 }
 
-                recordPointable = (ARecordPointable) pa.allocateRecordValue(recordType);
+                ARecordPointable recordPointable = (ARecordPointable) pa.allocateRecordValue(recordType);
                 recordPointable.set(outInput0.getByteArray(), outInput0.getStartOffset(), outInput0.getLength());
 
                 try {
-                    orderedListBuilder.reset(listType);
-                    processRecord(recordPointable, recordType);
-                    orderedListBuilder.write(out, true);
+                    processRecord(recordPointable, recordType, out);
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (AsterixException e) {
@@ -121,42 +139,68 @@ public class GetRecordFieldsEvalFactory implements ICopyEvaluatorFactory {
                 }
             }
 
-            public void processRecord(ARecordPointable recordAccessor, ARecordType recType) throws IOException,
-                    AsterixException {
+            public void processRecord(ARecordPointable recordAccessor, ARecordType recType, DataOutput out)
+                    throws IOException, AsterixException {
                 List<IVisitablePointable> fieldNames = recordAccessor.getFieldNames();
                 List<IVisitablePointable> fieldTags = recordAccessor.getFieldTypeTags();
                 List<IVisitablePointable> fieldValues = recordAccessor.getFieldValues();
 
-                ArrayBackedValueStorage fieldAbvs = new ArrayBackedValueStorage();
-                ArrayBackedValueStorage valueAbvs = new ArrayBackedValueStorage();
-                ArrayBackedValueStorage itemValue = new ArrayBackedValueStorage();
-                IARecordBuilder fieldRecordBuilder = new RecordBuilder();
+                ArrayBackedValueStorage fieldAbvs = getTempBuffer();
+                ArrayBackedValueStorage valueAbvs = getTempBuffer();
+                ArrayBackedValueStorage itemValue = getTempBuffer();
+
+                OrderedListBuilder orderedListBuilder = getOrderedListBuilder();
+                orderedListBuilder.reset(listType);
+                IARecordBuilder fieldRecordBuilder = getRecordBuilder();
                 fieldRecordBuilder.reset(null);
-                String fieldTypeName = null;
-                for (int i = 0; i < fieldNames.size() - 1; i++) {
+                for (int i = 0; i < fieldNames.size(); i++) {
                     itemValue.reset();
                     fieldRecordBuilder.init();
 
                     // write name
                     fieldAbvs.reset();
                     stringSerde.serialize(fieldName, fieldAbvs.getDataOutput());
-                    valueAbvs.reset();
-                    aString.setValue(recType.getFieldNames()[i]);
-                    stringSerde.serialize(aString, valueAbvs.getDataOutput());
-                    fieldRecordBuilder.addField(fieldAbvs, valueAbvs);
+                    fieldRecordBuilder.addField(fieldAbvs, fieldNames.get(i));
 
                     // write type
                     fieldAbvs.reset();
                     stringSerde.serialize(typeName, fieldAbvs.getDataOutput());
                     valueAbvs.reset();
-                    if (recType.getFieldTypes()[i].getTypeName() == null) {
-                        fieldTypeName = "UNKNOWN";
-                    } else {
-                        fieldTypeName = recType.getFieldTypes()[i].getTypeName();
-                    }
-                    aString.setValue(fieldTypeName);
+                    tag = EnumDeserializer.ATYPETAGDESERIALIZER.deserialize(fieldTags.get(i).getByteArray()[fieldTags
+                            .get(i).getStartOffset()]);
+                    aString.setValue(tag.toString());
                     stringSerde.serialize(aString, valueAbvs.getDataOutput());
                     fieldRecordBuilder.addField(fieldAbvs, valueAbvs);
+
+                    // write open
+                    fieldAbvs.reset();
+                    stringSerde.serialize(isOpenName, fieldAbvs.getDataOutput());
+                    valueAbvs.reset();
+                    currentFieldName = stringSerde
+                            .deserialize(new DataInputStream(new ByteArrayInputStream(fieldNames.get(i).getByteArray(),
+                                    fieldNames.get(i).getStartOffset(), fieldNames.get(i).getLength())));
+                    if (recType.getFieldType(currentFieldName.getStringValue()) != null) {
+                        booleanSerde.serialize(ABoolean.FALSE, valueAbvs.getDataOutput());
+                    } else {
+                        booleanSerde.serialize(ABoolean.TRUE, valueAbvs.getDataOutput());
+                    }
+                    fieldRecordBuilder.addField(fieldAbvs, valueAbvs);
+
+                    // write nested 
+                    if (tag == ATypeTag.RECORD) {
+                        fieldAbvs.reset();
+                        stringSerde.serialize(nestedName, fieldAbvs.getDataOutput());
+                        valueAbvs.reset();
+                        newType = (ARecordType) recType.getFieldType(currentFieldName.getStringValue());
+                        if (newType == null) {
+                            newType = openType;
+                        }
+                        ARecordPointable recordP = (ARecordPointable) pa.allocateRecordValue(newType);
+                        recordP.set(fieldValues.get(i).getByteArray(), fieldValues.get(i).getStartOffset(), fieldValues
+                                .get(i).getLength());
+                        processRecord(recordP, newType, valueAbvs.getDataOutput());
+                        fieldRecordBuilder.addField(fieldAbvs, valueAbvs);
+                    }
 
                     // write record
                     fieldRecordBuilder.write(itemValue.getDataOutput(), true);
@@ -164,7 +208,55 @@ public class GetRecordFieldsEvalFactory implements ICopyEvaluatorFactory {
                     // add item to the list of fields
                     orderedListBuilder.addItem(itemValue);
                 }
+                orderedListBuilder.write(out, true);
+
+                // Return memory.
+                returnRecordBuilder(fieldRecordBuilder);
+                returnOrderedListBuilder(orderedListBuilder);
+                returnTempBuffer(fieldAbvs);
+                returnTempBuffer(valueAbvs);
+                returnTempBuffer(itemValue);
             }
+
+            private IARecordBuilder getRecordBuilder() {
+                RecordBuilder recBuilder = (RecordBuilder) recordBuilderPool.poll();
+                if (recBuilder != null) {
+                    return recBuilder;
+                } else {
+                    return new RecordBuilder();
+                }
+            }
+
+            private void returnRecordBuilder(IARecordBuilder recBuilder) {
+                this.recordBuilderPool.add(recBuilder);
+            }
+
+            private OrderedListBuilder getOrderedListBuilder() {
+                OrderedListBuilder orderedListBuilder = (OrderedListBuilder) orderedListBuilderPool.poll();
+                if (orderedListBuilder != null) {
+                    return orderedListBuilder;
+                } else {
+                    return new OrderedListBuilder();
+                }
+            }
+
+            private void returnOrderedListBuilder(OrderedListBuilder orderedListBuilder) {
+                this.orderedListBuilderPool.add(orderedListBuilder);
+            }
+
+            private ArrayBackedValueStorage getTempBuffer() {
+                ArrayBackedValueStorage tmpBaaos = baaosPool.poll();
+                if (tmpBaaos != null) {
+                    return tmpBaaos;
+                } else {
+                    return new ArrayBackedValueStorage();
+                }
+            }
+
+            private void returnTempBuffer(ArrayBackedValueStorage tempBaaos) {
+                baaosPool.add(tempBaaos);
+            }
+
         };
     }
 }
