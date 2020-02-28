63c840c9c6f653d861e8e77dc8ea7c6dfe29c667
Ian Maxon <imaxon@uci.edu>
2020-02-27 16:32:27 -0800

    um...

--- entrypoint.py
+++ entrypoint.py
@@ -18,8 +18,12 @@ class Wrapper(object):
 
 # Make sure that the python code is started first.
 # Then execute: java -cp py4j.jar py4j.examples.SingleThreadClientApplication
+Pyro4.config.SERVERTYPE='multiplex'
+Pyro4.config.SERIALIZER='msgpack'
+Pyro4.config.SERIALIZERS_ACCEPTED = {'msgpack'}
 port = int(sys.argv[1])
 sent = Wrapper()
 d = Pyro4.Daemon(host="127.0.0.1",port=port)
 d.register(sent,"sentiment")
+print(Pyro4.config.dump())
 d.requestLoop()
