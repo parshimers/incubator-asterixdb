dd5c853d77b8df0dd9b292d057fa52606582b47c
Ian Maxon <ian@maxons.email>
2022-05-16 14:25:32 -0700

    wait properly

--- asterixdb/asterix-docker/src/test/java/org/apache/asterix/test/podman/PodmanPythonFunctionIT.java
+++ asterixdb/asterix-docker/src/test/java/org/apache/asterix/test/podman/PodmanPythonFunctionIT.java
@@ -3,7 +3,9 @@ package org.apache.asterix.test.podman;
 import java.net.InetSocketAddress;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.asterix.test.common.TestExecutor;
 import org.apache.asterix.test.runtime.ExecutionTestUtil;
@@ -30,19 +32,18 @@ import com.github.dockerjava.api.DockerClient;
 public class PodmanPythonFunctionIT {
     public static final DockerImageName ASTERIX_IMAGE = DockerImageName.parse("asterixdb/socktest");
     @ClassRule
-    public static GenericContainer<?> asterix = new GenericContainer(ASTERIX_IMAGE).withExposedPorts(19004,5006).withFileSystemBind("../asterix-app/","/var/tmp/asterix-app/", BindMode.READ_WRITE);
+    public static GenericContainer<?> asterix = new GenericContainer(ASTERIX_IMAGE).withExposedPorts(19004,5006,19002).withFileSystemBind("../asterix-app/","/var/tmp/asterix-app/", BindMode.READ_WRITE);
     protected static final String TEST_CONFIG_FILE_NAME = "../asterix-app/src/test/resources/cc.conf";
     private static final boolean cleanupOnStop = true;
 
 
     @BeforeClass
     public static void setUp() throws Exception {
-        final TestExecutor testExecutor = new TestExecutor();
-        //NO.
-        Thread.sleep(5000);
+        final TestExecutor testExecutor = new TestExecutor(List.of(InetSocketAddress.createUnresolved(asterix.getHost(), asterix.getMappedPort(19002))));
         asterix.execInContainer("/opt/setup.sh");
         LangExecutionUtil.setUp(TEST_CONFIG_FILE_NAME, testExecutor, false, true, new PodmanUDFLibrarian(asterix));
-        setNcEndpoints(testExecutor);
+        setEndpoints(testExecutor);
+        testExecutor.waitForClusterActive(60, TimeUnit.SECONDS);
     }
 
     @AfterClass
@@ -71,11 +72,11 @@ public class PodmanPythonFunctionIT {
         LangExecutionUtil.test(tcCtx);
     }
 
-    private static void setNcEndpoints(TestExecutor testExecutor) {
+    private static void setEndpoints(TestExecutor testExecutor) {
         final Map<String, InetSocketAddress> ncEndPoints = new HashMap<>();
         final String ip = asterix.getHost();
         final String nodeId = "asterix_nc";
-        int apiPort = asterix.getFirstMappedPort();
+        int apiPort = asterix.getMappedPort(19004);
         ncEndPoints.put(nodeId, InetSocketAddress.createUnresolved(ip, apiPort));
         testExecutor.setNcEndPoints(ncEndPoints);
     }
