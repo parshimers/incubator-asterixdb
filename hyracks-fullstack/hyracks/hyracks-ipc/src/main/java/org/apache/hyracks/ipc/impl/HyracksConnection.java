88c48464783f3c9c626636a411d83a6e643bcdd6
Dmitry Lychagin <dmitry.lychagin@couchbase.com>
2022-06-10 12:44:25 -0700

    [NO ISSUE][COMP] Introduce ANALYZE DATASET statement
    - user model changes: yes
    - storage format changes: no
    - interface changes: no
    
    Details:
    - Introduce ANALYZE DATASET statement that
      creates a sample of the original dataset
    - Add SampleDataSource to read the sample
    - Make RemoveRedundantVariablesRule ignore
      variables that are marked as non-inlinable
    
    Change-Id: Ibeb6228f2dc8edbf642e61cd5634c71933a18971
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/16324
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Ali Alsuliman <ali.al.solaiman@gmail.com>

--- hyracks-fullstack/hyracks/hyracks-ipc/src/main/java/org/apache/hyracks/ipc/impl/HyracksConnection.java
+++ hyracks-fullstack/hyracks/hyracks-ipc/src/main/java/org/apache/hyracks/ipc/impl/HyracksConnection.java
@@ -52,6 +52,7 @@ import org.apache.hyracks.api.job.JobId;
 import org.apache.hyracks.api.job.JobInfo;
 import org.apache.hyracks.api.job.JobSpecification;
 import org.apache.hyracks.api.job.JobStatus;
+import org.apache.hyracks.api.job.profiling.IOperatorStats;
 import org.apache.hyracks.api.network.ISocketChannelFactory;
 import org.apache.hyracks.api.topology.ClusterTopology;
 import org.apache.hyracks.api.util.InvokeUtil;
@@ -188,6 +189,17 @@ public final class HyracksConnection implements IHyracksClientConnection {
     public void waitForCompletion(JobId jobId) throws Exception {
         try {
             hci.waitForCompletion(jobId);
+        } catch (InterruptedException e) {
+            // Cancels an on-going job if the current thread gets interrupted.
+            cancelJob(jobId);
+            throw e;
+        }
+    }
+
+    @Override
+    public List<IOperatorStats> waitForCompletion(JobId jobId, List<String> statOperatorNames) throws Exception {
+        try {
+            return hci.waitForCompletion(jobId, statOperatorNames);
         } catch (InterruptedException e) {
             // Cancels an on-going job if the current thread gets interrupted.
             cancelJob(jobId);
